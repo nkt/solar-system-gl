6c1f5a75a90a2d6b1dbaba63c84881b33294154a
Nikita Gusakov <dev@nkt.me>
2015-06-12 15:51:24 +0300

    Introduce planet_system

--- solar-system/main.c
+++ solar-system/main.c
@@ -17,31 +17,26 @@
 #define HEX(c) HEX_RED(c), HEX_GREEN(c), HEX_BLUE(c)
 
 #define TIMER_DELAY 1
-#define RADIUS_RATIO 2
-#define SUN_RADIUS 696.342
-#define BASE_RADIUS (SUN_RADIUS / RADIUS_RATIO) + 70
-#define PLANET_COUNT 4
 
 void timer(int);
 void display();
 void reshape(int, int);
 
-struct planet sun, mercury, venus, earth, mars;
-struct planet *planets;
+struct planet_system ps;
 
 int main(int argc, char **argv) {
-    init_planet(&sun, 0xfc9a24, 100, 0);
-    init_planet(&mercury, 0x99989D, 24.39, 0.01);
-    init_planet(&venus, 0x6894D2, 60.51, 0.002);
-    init_planet(&earth, 0x005db3, 63.71, 0.0035);
-    init_planet(&mars, 0xD0331A, 33.89, 0.0015);
+    struct planet sun, mercury, venus, earth, mars;
+    init_planet(&sun, 0xfc9a24, 695800, 0, 0);
+    init_planet(&mercury, 0x99989D, 2440, 57910000, 0.01);
+    init_planet(&venus, 0x6894D2, 6052, 108200000, 0.002);
+    init_planet(&earth, 0x005db3, 6371, 149600000, 0.0035);
+    init_planet(&mars, 0xD0331A, 3390, 227900000, 0.0015);
     
-    planets = (struct planet*) malloc(sizeof(struct planet) * PLANET_COUNT);
-    planets[0] = mercury;
-    planets[1] = venus;
-    planets[2] = earth;
-    planets[3] = mars;
-
+    init_planet_system(&ps, sun);
+    add_planet(&ps, mercury);
+    add_planet(&ps, venus);
+    add_planet(&ps, earth);
+    add_planet(&ps, mars);
     
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
@@ -58,17 +53,8 @@ int main(int argc, char **argv) {
 
 
 void timer(int a) {
-    int radius = 0;
-    sun.angle += 0.5;
-    for (int i = 0; i < PLANET_COUNT; i++, radius += 100) {
-        int planet_radius = (BASE_RADIUS + radius);
+    calc_planet_system_state(&ps);
 
-        planets[i].x = planet_radius * cos(planets[i].fi);
-        planets[i].y = planet_radius * sin(planets[i].fi);
-        planets[i].angle += 1.5;
-        planets[i].fi += planets[i].fi_step;
-    }
-    
     glutPostRedisplay();
     glutTimerFunc(TIMER_DELAY, timer, 0);
 }
@@ -79,7 +65,7 @@ void display_planet(struct planet p) {
     glTranslatef(p.x, p.y, p.z);
     glRotatef(p.angle, 15, 25, 1);
     int lines = p.radius / 3;
-    glutWireSphere(p.radius / RADIUS_RATIO, lines, lines);
+    glutWireSphere(p.radius, lines, lines);
     glPopMatrix();
 }
 
@@ -87,10 +73,10 @@ void display() {
     glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
     glLoadIdentity();
     
-    display_planet(sun);
+    display_planet(ps.center);
     
-    for (int i = 0; i < PLANET_COUNT; i++) {
-        display_planet(planets[i]);
+    for (int i = 0; i < ps.size; i++) {
+        display_planet(ps.planets[i]);
     }
     
     glutSwapBuffers();
--- solar-system/planet.h
+++ solar-system/planet.h
@@ -1,3 +1,9 @@
+#include <stdlib.h>
+
+#define RADIUS_RATIO 100
+#define DISTANCE_RATIO 250000
+#define CENTER_RADIUS_RATIO ((DISTANCE_RATIO / RADIUS_RATIO) * 1.5)
+
 struct planet {
     int color;
     double x;
@@ -7,12 +13,46 @@ struct planet {
     double fi_step;
     double angle;
     double radius;
+    double distance;
 };
 
-void init_planet(struct planet *item, int color, double radius, double fi_step) {
+void init_planet(struct planet *item, int color, double radius, double distance, double fi_step) {
     item->color = color;
     item->radius = radius;
     item->fi_step = fi_step;
     item->x = item->y = item->z = item->fi = 0;
     item->angle = 0;
+    item->distance = distance;
+}
+
+
+struct planet_system {
+    struct planet center;
+    struct planet *planets;
+    int size;
+};
+
+void init_planet_system(struct planet_system *item, struct planet center) {
+    center.radius /= CENTER_RADIUS_RATIO;
+    item->center = center;
+    item->size = 0;
+    item->planets = NULL;
+}
+
+void add_planet(struct planet_system *item, struct planet new_planet) {
+    item->size++;
+    item->planets = (struct planet *) realloc(item->planets, sizeof(struct planet) * item->size);
+    new_planet.radius /= RADIUS_RATIO;
+    new_planet.distance /= DISTANCE_RATIO;
+    item->planets[item->size - 1] = new_planet;
+}
+
+void calc_planet_system_state(struct planet_system *item) {
+    item->center.angle += 0.5;
+    for (int i = 0; i < item->size; i++) {
+        item->planets[i].x = item->planets[i].distance * cos(item->planets[i].fi);
+        item->planets[i].y = item->planets[i].distance * sin(item->planets[i].fi);
+        item->planets[i].angle += 1.5;
+        item->planets[i].fi += item->planets[i].fi_step;
+    }
 }
